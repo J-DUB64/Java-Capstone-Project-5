f8dd4354b3335dec31a6349990730e44c8b66c9a
nyckolle-lucuab <nyckolle.lucuab@tlgcohort.com>
2023-01-10 15:42:37 -0800

    Update Player & PlayerTest class to include ArrayList of Ships

--- src/test/java/com/tlglearning/battleship/PlayerTest.java
+++ src/test/java/com/tlglearning/battleship/PlayerTest.java
@@ -2,6 +2,8 @@ package com.tlglearning.battleship;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.tlglearning.battleship.Ship.Direction;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
@@ -10,6 +12,13 @@ class PlayerTest {
   private static final Player PLAYER_1 = new Player("Evelyn", PLAYER_1_BOARD);
   private static final String PLAYER_1_NAME = "Evelyn";
   private static final int PLAYER_1_BOARD_REMAINING_SHIPS = 0;
+  public static final ArrayList<Ship> EMPTY_SHIP_INVENTORY = new ArrayList<>();
+  public static final ArrayList<Ship> PLAYER_1_SHIP_INVENTORY = new ArrayList<>() {
+    {
+      add(new Ship(ShipType.CARRIER, new Position(1, 3), Direction.HORIZONTAL));
+    }
+  };
+
 
   @Test
   void getName_returnsPlayerName() {
@@ -25,4 +34,23 @@ class PlayerTest {
   void getShipsRemaining_returnsShipsRemaining() {
     assertEquals(PLAYER_1_BOARD_REMAINING_SHIPS, PLAYER_1.getShipsRemaining());
   }
+
+  @Test
+  void getPlayerShipInventory_returnsEmptyIntialArrayList() {
+    assertEquals(
+        EMPTY_SHIP_INVENTORY,
+        PLAYER_1.getPlayerShipInventory());
+  }
+
+//  @Test
+//  void getPlayerShipInventory_returnsArrayListWithBattleship() {
+//    PLAYER_1.getPlayerShipInventory().add(
+//        new Ship(ShipType.CARRIER, new Position(1, 3), Direction.HORIZONTAL)
+//    );
+//
+//    assertEquals(
+//        PLAYER_1_SHIP_INVENTORY,
+//        PLAYER_1.getPlayerShipInventory()
+//    );
+//  }
 }
