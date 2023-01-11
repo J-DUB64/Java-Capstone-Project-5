cdcb5ff94d3229c0feda0cbd26990f158681a57f
nyckolle-lucuab <nyckolle.lucuab@tlgcohort.com>
2023-01-11 10:47:16 -0800

    Refactored input logic for ship placement to handle IllegalArgumentException + created GameStateTest file

---
+++ src/test/java/com/tlglearning/battleship/GameStateTest.java
@@ -0,0 +1,20 @@
+package com.tlglearning.battleship;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameStateTest {
+
+  @Test
+  void placePlayerShips() {
+  }
+
+  @Test
+  void computerShoots() {
+  }
+
+  @Test
+  void setGameOver() {
+  }
+}
